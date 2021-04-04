f205251a1928aa7cb5b0a7a002095687ac813be0
Sara <41219424+asloth@users.noreply.github.com>
2021-04-04 04:26:39 -0500

    conecta exitosamente a firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
 import 'package:emcare/src/presentation/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EmCare',
-      home: LogIn(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return MaterialApp(
+            title: 'EmCare',
+            home: Text('hasError'),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'EmCare',
+            home: LogIn(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return MaterialApp(
+          title: 'EmCare',
+          home: Text('Loading'),
+        );
+        ;
+      },
     );
   }
 }
