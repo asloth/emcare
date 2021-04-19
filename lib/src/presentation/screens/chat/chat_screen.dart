3187bb345b8c3d0cce704e65daf9de6f83e4950a
Sara <41219424+asloth@users.noreply.github.com>
2021-04-19 02:25:32 -0500

    ya se puede establecer una conversacion

--- lib/src/presentation/screens/chat/chat_screen.dart
+++ lib/src/presentation/screens/chat/chat_screen.dart
@@ -1,49 +1,121 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:emcare/constants.dart';
+import 'package:emcare/src/presentation/screens/chat/widgets/message.dart';
 import 'package:flutter/material.dart';
 
-class Chat extends StatelessWidget {
+import 'package:intl/intl.dart';
+
+class Chat extends StatefulWidget {
   const Chat({Key key}) : super(key: key);
 
   @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  final messageInsert = TextEditingController();
+  List<Map> messsages = [];
+
+  void response(query) async {
+    DialogAuthCredentials credentials =
+        await DialogAuthCredentials.fromFile("assets/df_service.json");
+
+    DialogFlowtter instance = DialogFlowtter(
+      credentials: credentials,
+    );
+
+    DetectIntentResponse response = await instance.detectIntent(
+      queryInput: QueryInput(
+        text: TextInput(
+          text: query,
+          languageCode: "es",
+        ),
+      ),
+    );
+
+    setState(() {
+      messsages.insert(0, {
+        "data": 0,
+        "message": response.text,
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController newMessage = new TextEditingController();
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Container(
         child: Column(
           children: [
-            Spacer(),
-            ListTile(
-                title: Container(
-                  height: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    color: kBackgroundColor2,
-                  ),
-                  padding: EdgeInsets.only(left: 15),
-                  child: TextFormField(
-                    controller: newMessage,
-                    decoration: InputDecoration(
-                      hintText: "Enter a Message...",
-                      hintStyle: TextStyle(color: Colors.black26),
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                    ),
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                    onChanged: (value) {},
-                  ),
+            Container(
+              padding: EdgeInsets.only(top: 15, bottom: 10),
+              child: Text(
+                "Hoy, ${DateFormat("Hm").format(DateTime.now())}",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Flexible(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: (messsages.length == null) ? 0 : messsages.length,
+                itemBuilder: (context, index) => Messages(
+                  textmessage: messsages[index]["message"].toString(),
+                  data: messsages[index]["data"],
                 ),
-                trailing: IconButton(
-                  icon: Icon(
-                    Icons.send,
-                    size: 30.0,
-                    color: kPrimaryColor,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Divider(),
+            ListTile(
+              title: Container(
+                height: 35,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  color: kBackgroundColor2,
+                ),
+                padding: EdgeInsets.only(left: 15),
+                child: TextFormField(
+                  controller: messageInsert,
+                  decoration: InputDecoration(
+                    hintText: "Escribe un mensaje...",
+                    hintStyle: TextStyle(color: Colors.black26),
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
                   ),
-                  onPressed: () {},
-                )),
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                  onChanged: (value) {},
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.send,
+                  size: 30.0,
+                  color: kPrimaryColor,
+                ),
+                onPressed: () {
+                  if (messageInsert.text.isEmpty) {
+                    print("empty message");
+                  } else {
+                    setState(() {
+                      messsages.insert(
+                          0, {"data": 1, "message": messageInsert.text});
+                    });
+                    response(messageInsert.text);
+                    messageInsert.clear();
+                  }
+                  FocusScopeNode currentFocus = FocusScope.of(context);
+                  if (!currentFocus.hasPrimaryFocus) {
+                    currentFocus.unfocus();
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/src/presentation/screens/chat/widgets/message.dart
@@ -0,0 +1,71 @@
+import 'package:bubble/bubble.dart';
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  final String textmessage;
+  final int data;
+
+  const Messages({
+    Key key,
+    @required this.textmessage,
+    @required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment:
+            data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          data == 0
+              ? Container(
+                  height: 60,
+                  width: 60,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/imgs/robot.jpg"),
+                  ),
+                )
+              : Container(),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Bubble(
+                radius: Radius.circular(15.0),
+                color: data == 0
+                    ? Color.fromRGBO(23, 157, 139, 1)
+                    : Colors.orangeAccent,
+                elevation: 0.0,
+                child: Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Flexible(
+                          child: Container(
+                        constraints: BoxConstraints(maxWidth: 200),
+                        child: Text(
+                          textmessage,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ))
+                    ],
+                  ),
+                )),
+          ),
+          data == 1
+              ? Container(
+                  height: 60,
+                  width: 60,
+                  child: CircleAvatar(),
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
